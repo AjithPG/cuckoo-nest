6d5bcf95793d4671df84f79862e769d85fa128a2
ajithpg <ajithpg008@gmail.com>
2025-06-13 22:21:37 +0530

    payload integraion

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+  });
+  return <main>{JSON.stringify(data, null, 2)}</main>;
+}
