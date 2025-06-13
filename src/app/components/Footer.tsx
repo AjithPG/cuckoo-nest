33cba58f73bb5f9ec3099db94ada601156b41148
ajithpg <ajithpg008@gmail.com>
2025-06-13 19:31:43 +0530

    feat:add footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,11 @@
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="flex border-t justify-between p-6">
+     <div className="flex items-center gap-2">
+       <p>Footer</p> 
+     </div> 
+    </footer>
   )
 }
 
